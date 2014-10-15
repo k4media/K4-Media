74691e5d0b575a11fd68f7226db27006efac8167
Robert <robert@k4media.com>
2014-10-15 16:48:10 +0700

    Footer.php
    another test

---
+++ NewProject/footer.php
@@ -0,0 +1,51 @@
+<div id='footer'>
+	<span> <center> All content © Forum Syd unless otherwise noted.</center></span>
+	<?php /* 
+	
+	<!-- footer logo -->
+	<div class='section group'>
+		<div class='col span_4_of_4'>
+			<div id="footer-logo"><?php echo "<img style='width: 219px; height: 27px' src='" . get_stylesheet_directory_uri() . "/images/forum-syd-logo.png' title='" . get_bloginfo( 'description' ) . "' >";?></div>
+<h2> echo '  All content © Forum Syd unless otherwise noted.';</h2>
+		</div>
+	</div>
+	
+	<!-- about us info -->
+	<div class='section group'>
+		<div class='col span_2_of_4'>
+				<h4>About Forum Syd</h4>
+				<p class='footer-text'>Forum Syd is a Swedish non governmental member organisation. We work with people and people's rights. Forum Syd believes that only when peoples rights are being recognized sustainable and long-term development can take place.</p>
+			</div>
+		
+		<div class='col span_2_of_4'>
+			<h4>Visit our offices</h4>
+			<ul>
+				<li><a href='http://forumsyd.org/InternationalStart/Belarus/'>Belarus</a></li>
+				<li><a href='http://forumsyd.org/InternationalStart/Colombia/'>Colombia</a></li>
+				<li><a href='http://forumsyd.org/InternationalStart/Kenya/'>Kenya</a></li>
+				<li><a href='http://forumsyd.org/'>Sweden</a></li>
+				<li><a href='http://forumsyd.org/InternationalStart/Tanzania/'>Tanzania</a></li>
+			</ul>
+		</div>
+	</div>
+
+
+		*/
+	?>
+
+</div><!-- /footer -->
+
+</div><!-- /main-->
+
+<?php wp_footer(); ?>
+
+
+<!-- Site Meter XHTML Strict 1.0 -->
+<div style='visibility: hidden'><script type="text/javascript" src="http://s30.sitemeter.com/js/counter.js?site=s30forumsydcambodia">
+</script>
+</div>
+<!-- Copyright (c)2009 Site Meter -->
+
+
+</body>
+</html>
