15870e19c765046b0d20bb1c0deb0e9b10845495
Robert <k4media@users.noreply.github.com>
2014-10-15 16:55:56 +0700

    Update footer.php

--- NewProject/footer.php
+++ NewProject/footer.php
@@ -1,13 +1,7 @@
 	<div id='footer'>
-
 		<span> <center> All content © Forum Syd unless otherwise noted.</center></span>
-
 	</div><!-- /footer -->
-
 </div><!-- /main-->
-
 <?php wp_footer(); ?>
-
 </body>
-
 </html>
